7a728ee3e6bd6ea74a64175ae9b349fc3996d6f4
alesande <alesande@student.21-school.ru>
2025-02-24 19:38:32 +0300

    updated e2e

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,65 +1,17 @@
 package main
 
 import (
+	"AvitoWinter/internal/app"
 	testClient "AvitoWinter/test/e2e/client"
 	"context"
+	"database/sql"
 	log2 "github.com/sirupsen/logrus"
-)
+	_ "github.com/stretchr/testify"
+	_ "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
 
-//func main() {
-//	ctx, cancel := context.WithCancel(context.Background())
-//	defer cancel()
-//
-//	c := testClient.NewTestClient()
-//
-//	body := strings.NewReader(`{"username": "alesande123", "password": "alesande12@"}`)
-//
-//	authResponse, err := c.PostApiAuthWithBody(ctx, "application/json", body)
-//	if err != nil {
-//		log2.Errorf("PostApiAuthWithBody:%v", err)
-//		return
-//	}
-//
-//	if authResponse == nil {
-//		log2.Printf("authResponse == nil")
-//		return
-//	}
-//
-//	//authResponse.JSON200.Token
-//	log2.Printf("Status: %d", authResponse.StatusCode())
-//	log2.Printf("Body: %s", string(authResponse.Body))
-//
-//	r := bytes.NewReader(authResponse.Body)
-//	err = json.NewDecoder(r).Decode(&authResponse.JSON200)
-//	if err != nil {
-//		log2.Printf("Decode error")
-//		return
-//	}
-//
-//	token := authResponse.JSON200
-//
-//	if authResponse == nil {
-//		log2.Printf("authResponse == nil")
-//		return
-//	}
-//
-//	if token == nil {
-//		log2.Printf("token == nil")
-//		return
-//	}
-//
-//	if token.Token == nil {
-//		log2.Printf("token.Token == nil")
-//		return
-//	}
-//
-//	if len(*token.Token) > 0 {
-//		log2.Printf("Token: %v", *token.Token)
-//	} else {
-//		log2.Printf("Error get token: %v", *token.Token)
-//	}
-//
-//}
+	"testing"
+)
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -89,13 +41,6 @@ func main() {
 	log2.Printf("Status: %d", authResponse.StatusCode())
 	log2.Printf("Body: %s", string(authResponse.Body))
 
-	//r := bytes.NewReader(authResponse.Body)
-	//err = json.NewDecoder(r).Decode(&authResponse.JSON200)
-	//if err != nil {
-	//	log2.Printf("Decode error")
-	//	return
-	//}
-
 	token := authResponse.JSON200
 
 	if authResponse == nil {
@@ -118,5 +63,52 @@ func main() {
 	} else {
 		log2.Printf("Error get token: %v", *token.Token)
 	}
+
+}
+
+var (
+	db        *sql.DB
+	serverURL = "http://localhost:8080/"
+)
+
+func TestMain(m *testing.M) {
+	setupTestDB()
+	runService()
+	m.Run()
+}
+
+func setupTestDB() (*sql.DB, error) {
+	ctx := context.Background()
+	container, err := postgres.Run(ctx, "postgres:15", postgres.WithDatabase("test_db"),
+		postgres.WithUsername("user"), postgres.WithPassword("password"))
+	if err != nil {
+		panic(err)
+	}
+
+	conn, _ := container.ConnectionString(ctx)
+	db, err = sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func truncateTables() {
+	//db.Exec("TRUNCATE products RESTART IDENTITY CASCADE")
+}
+
+func runService() {
+	go func() {
+		err := app.RunApp()
+		if err != nil {
+			log2.Errorf("app.RunApp%v", err)
+			return
+		}
+	}()
+
+}
+
+func GetToken(t *testing.T) {
 
 }
